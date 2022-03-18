861c47c67473d575499b8f48e8efbf9feb977a84
James Morse <james.morse@arm.com>
2022-03-18 17:48:37 +0000

    arm64: proton-pack: Report Spectre-BHB vulnerabilities as part of Spectre-v2
    commit dee435be76f4117410bbd90573a881fd33488f37 upstream.
    
    Speculation attacks against some high-performance processors can
    make use of branch history to influence future speculation as part of
    a spectre-v2 attack. This is not mitigated by CSV2, meaning CPUs that
    previously reported 'Not affected' are now moderately mitigated by CSV2.
    
    Update the value in /sys/devices/system/cpu/vulnerabilities/spectre_v2
    to also show the state of the BHB mitigation.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    [ code move to cpu_errata.c for backport ]
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -1173,9 +1173,9 @@ bool is_spectre_bhb_affected(const struct arm64_cpu_capabilities *entry,
 
 	if (supports_csv2p3(scope))
 		return false;
-	
+
 	if (supports_clearbhb(scope))
-		return true;	
+		return true;
 
 	if (spectre_bhb_loop_affected(scope))
 		return true;
@@ -1218,7 +1218,7 @@ static const char *kvm_bhb_get_vecs_end(const char *start)
 	else if (start == __spectre_bhb_loop_k32_start)
 		return __spectre_bhb_loop_k32_end;
 	else if (start == __spectre_bhb_clearbhb_start)
-		return __spectre_bhb_clearbhb_end;	
+		return __spectre_bhb_clearbhb_end;
 
 	return NULL;
 }
@@ -1280,8 +1280,8 @@ void spectre_bhb_enable_mitigation(const struct arm64_cpu_capabilities *entry)
 		state = SPECTRE_MITIGATED;
 	} else if (supports_clearbhb(SCOPE_LOCAL_CPU)) {
 		kvm_setup_bhb_slot(__spectre_bhb_clearbhb_start);
-		this_cpu_set_vectors(EL1_VECTOR_BHB_CLEAR_INSN);	
-		
+		this_cpu_set_vectors(EL1_VECTOR_BHB_CLEAR_INSN);
+
 		state = SPECTRE_MITIGATED;
 	} else if (spectre_bhb_loop_affected(SCOPE_LOCAL_CPU)) {
 		switch (spectre_bhb_loop_affected(SCOPE_SYSTEM)) {
