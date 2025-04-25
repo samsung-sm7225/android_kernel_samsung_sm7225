861073d725f40538c9fc315b50ba259c849b8d8b
Aflaungos <vimavick@gmail.com>
2025-04-25 16:49:48 -0300

    sm7225: nt36523_spi: Fix proximity function
    * This is leftover code from a bad commit.

--- drivers/input/touchscreen/novatek/nt36523_spi/nt36xxx.c
+++ drivers/input/touchscreen/novatek/nt36523_spi/nt36xxx.c
@@ -2398,7 +2398,6 @@ void nvt_ts_proximity_report(uint8_t *data)
 	}
 
 	status = p_event_proximity->status;
-	status = status == 5 || !status;
 
 	input_info(true, &ts->client->dev,"proximity->status = %d\n", status);
 
