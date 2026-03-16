24b51f32149c6e4b097eb45a89264770f07fd165
Aflaungos <vimavick@gmail.com>
2026-03-16 01:04:17 -0300

    drivers/staging: qcacld-3.0: Add Samsung M236BXXSBEYH2 changes
    Change-Id: Ib40a32b392a541ea47ea824ae925739cd305f063

--- components/mlme/dispatcher/inc/cfg_mlme_generic.h
+++ components/mlme/dispatcher/inc/cfg_mlme_generic.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -738,6 +739,31 @@
 				0, 0x53, 0x49, CFG_VALUE_OR_DEFAULT, \
 				"Bit mask to retry Auth and full connection on assoc timeout to same AP for SAE connection")
 
+/*
+ * <ini>
+ * tx_retry_multiplier - TX retry multiplier
+ * @Min: 0
+ * @Max: 500
+ * @Default: 0
+ *
+ * This ini is used to indicate percentage to max retry limit to fw
+ * which can further be used by fw to multiply counter by
+ * tx_retry_multiplier percent.
+ *
+ * Supported Feature: STa/drivers/staging/qcacld-3.0/SAP
+ *
+ * Usage: External
+ *
+ * </ini>
+ */
+#define CFG_TX_RETRY_MULTIPLIER CFG_INI_UINT( \
+	"tx_retry_multiplier", \
+	0, \
+	500, \
+	0, \
+	CFG_VALUE_OR_DEFAULT, \
+	"percentage of max retry limit")
+
 #define CFG_GENERIC_ALL \
 	CFG(CFG_ENABLE_DEBUG_PACKET_LOG) \
 	CFG(CFG_PMF_SA_QUERY_MAX_RETRIES) \
@@ -768,5 +794,6 @@
 	CFG(CFG_MGMT_RETRY_MAX) \
 	CFG(CFG_BMISS_SKIP_FULL_SCAN) \
 	CFG(CFG_ENABLE_RING_BUFFER) \
-	CFG(CFG_SAE_CONNECION_RETRIES)
+	CFG(CFG_SAE_CONNECION_RETRIES) \
+	CFG(CFG_TX_RETRY_MULTIPLIER)
 #endif /* __CFG_MLME_GENERIC_H */
--- core/mac/src/pe/lim/lim_utils.c
+++ core/mac/src/pe/lim/lim_utils.c
@@ -1872,22 +1872,16 @@ static void __lim_process_channel_switch_timeout(struct pe_session *pe_session)
 		}
 
 		/*
-		 * If the channel-list that AP is asking us to switch is invalid
-		 * then we cannot switch the channel. Just disassociate from AP.
-		 * We will find a better AP !!!
+		 * The channel switch request received from AP is carrying
+		 * invalid channel. It's ok to ignore this channel switch
+		 * request as it might be from spoof AP. If it's from genuine
+		 * AP, it may lead to heart beat failure and result in
+		 * disconnection. DUT can go ahead and reconnect to it/any
+		 * other AP once it disconnects.
 		 */
-		if ((pe_session->limMlmState ==
-		   eLIM_MLM_LINK_ESTABLISHED_STATE) &&
-		   (pe_session->limSmeState != eLIM_SME_WT_DISASSOC_STATE) &&
-		   (pe_session->limSmeState != eLIM_SME_WT_DEAUTH_STATE)) {
-			pe_err("Invalid channel! Disconnect");
-			lim_tear_down_link_with_ap(mac,
-					   mac->lim.lim_timers.
-					   gLimChannelSwitchTimer.sessionId,
-					   eSIR_MAC_UNSUPPORTED_CHANNEL_CSA,
-					   eLIM_LINK_MONITORING_DISASSOC);
-			return;
-		}
+		pe_err("Invalid channel freq %u Ignore CSA request",
+		       channel_freq);
+		return;
 	}
 	switch (pe_session->gLimChannelSwitch.state) {
 	case eLIM_CHANNEL_SWITCH_PRIMARY_ONLY:
@@ -8872,64 +8866,13 @@ QDF_STATUS lim_pre_vdev_start(struct mac_context *mac,
 			      struct pe_session *session)
 {
 	struct wlan_channel *des_chan;
-	enum reg_wifi_band band;
-	uint8_t band_mask;
-	struct ch_params ch_params = {0};
-	qdf_freq_t sec_chan_freq = 0;
-
-	band = wlan_reg_freq_to_band(session->curr_op_freq);
-	band_mask = 1 << band;
-
-	ch_params.ch_width = session->ch_width;
-	ch_params.mhz_freq_seg0 =
-		wlan_reg_chan_band_to_freq(mac->pdev,
-					   session->ch_center_freq_seg0,
-					   band_mask);
-
-	if (session->ch_center_freq_seg1)
-		ch_params.mhz_freq_seg1 =
-			wlan_reg_chan_band_to_freq(mac->pdev,
-						   session->ch_center_freq_seg1,
-						   band_mask);
-
-	if (band == (REG_BAND_2G) && (ch_params.ch_width == CH_WIDTH_40MHZ)) {
-		if (ch_params.mhz_freq_seg0 ==  session->curr_op_freq + 10)
-			sec_chan_freq = session->curr_op_freq + 20;
-		if (ch_params.mhz_freq_seg0 ==  session->curr_op_freq - 10)
-			sec_chan_freq = session->curr_op_freq - 20;
-	}
-
-	wlan_reg_set_channel_params_for_freq(mac->pdev, session->curr_op_freq,
-					     sec_chan_freq, &ch_params);
-
-	pe_debug("vdev id %d freq %d seg0 %d seg1 %d ch_width %d cac_duration_ms %d beacon_interval %d hidden_ssid: %d dtimPeriod %d slot_time %d bcn tx rate %d mhz seg0 %d mhz seg1 %d",
-		 session->vdev_id, session->curr_op_freq,
-		 ch_params.center_freq_seg0,
-		 ch_params.center_freq_seg1, ch_params.ch_width,
-		 session->cac_duration_ms,
-		 session->beaconParams.beaconInterval,
-		 session->ssidHidden, session->dtimPeriod,
-		 session->shortSlotTimeSupported,
-		 session->beacon_tx_rate,
-		 ch_params.mhz_freq_seg0,
-		 ch_params.mhz_freq_seg1);
-	/* Invalid channel width means no suitable channel bonding in current
-	 * regdomain for requested channel frequency. Abort vdev start.
-	 */
-	if (ch_params.ch_width == CH_WIDTH_INVALID) {
-		pe_debug("abort vdev start invalid chan parameters");
-		return QDF_STATUS_E_INVAL;
-	}
 
 	des_chan = mlme_obj->vdev->vdev_mlme.des_chan;
 	des_chan->ch_freq = session->curr_op_freq;
-	des_chan->ch_width = ch_params.ch_width;
-	des_chan->ch_freq_seg1 = ch_params.center_freq_seg0;
-	des_chan->ch_freq_seg2 = ch_params.center_freq_seg1;
+	des_chan->ch_width = session->ch_width;
+	des_chan->ch_freq_seg1 = session->ch_center_freq_seg0;
+	des_chan->ch_freq_seg2 = session->ch_center_freq_seg1;
 	des_chan->ch_ieee = wlan_reg_freq_to_chan(mac->pdev, des_chan->ch_freq);
-	session->ch_width = ch_params.ch_width;
-	session->ch_center_freq_seg0 = ch_params.center_freq_seg0;
-	session->ch_center_freq_seg1 = ch_params.center_freq_seg1;
 
 	mlme_obj->mgmt.generic.maxregpower = session->maxTxPower;
 	mlme_obj->proto.generic.beacon_interval =
@@ -8941,6 +8884,11 @@ QDF_STATUS lim_pre_vdev_start(struct mac_context *mac,
 	mlme_obj->proto.generic.dtim_period = session->dtimPeriod;
 	mlme_obj->proto.generic.slot_time = session->shortSlotTimeSupported;
 	mlme_obj->mgmt.rate_info.bcn_tx_rate = session->beacon_tx_rate;
+
+	pe_debug("cac_duration_ms %d beacon_interval %d hidden_ssid: %d dtimPeriod %d slot_time %d bcn tx rate %d",
+		 session->cac_duration_ms, session->beaconParams.beaconInterval,
+		 session->ssidHidden, session->dtimPeriod,
+		 mlme_obj->proto.generic.slot_time, session->beacon_tx_rate);
 
 	mlme_obj->proto.ht_info.allow_ht = !!session->htCapability;
 	mlme_obj->proto.vht_info.allow_vht = !!session->vhtCapability;
