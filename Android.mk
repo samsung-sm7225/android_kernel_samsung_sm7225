1c4f0d07c0f2d3cb9f9aa6fa8e3b251c6cea25f2
Aflaungos <vimavick@gmail.com>
2026-03-15 23:28:42 -0300

    Squashed 'drivers/staging/qcacld-3.0/' content from commit 85849b032e51
    git-subtree-dir: drivers/staging/qcacld-3.0
    git-subtree-split: 85849b032e519219d47d9165244c3b6b9629f2bd

--- Android.mk
+++ Android.mk
@@ -1,79 +1,180 @@
-# Android makefile for audio kernel modules
-MY_LOCAL_PATH := $(call my-dir)
-
-UAPI_OUT := $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/include
-
-ifeq ($(call is-board-platform-in-list,msmnile $(MSMSTEPPE) $(TRINKET) kona lito bengal sdmshrike sdm660),true)
-$(shell mkdir -p $(UAPI_OUT)/linux;)
-$(shell mkdir -p $(UAPI_OUT)/sound;)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/ipc/Module.symvers)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/dsp/Module.symvers)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/dsp/codecs/Module.symvers)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/soc/Module.symvers)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/Module.symvers)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/Module.symvers)
-
-include $(MY_LOCAL_PATH)/include/uapi/Android.mk
-include $(MY_LOCAL_PATH)/ipc/Android.mk
-include $(MY_LOCAL_PATH)/dsp/Android.mk
-include $(MY_LOCAL_PATH)/dsp/codecs/Android.mk
-include $(MY_LOCAL_PATH)/soc/Android.mk
-include $(MY_LOCAL_PATH)/asoc/Android.mk
-include $(MY_LOCAL_PATH)/asoc/codecs/Android.mk
-endif
-
-ifeq ($(call is-board-platform-in-list,msmnile $(MSMSTEPPE) $(TRINKET) sdmshrike),true)
-ifneq ($(TARGET_BOARD_AUTO),true)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd934x/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/wcd934x/Android.mk
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/soc/Module.symvers)
-include $(MY_LOCAL_PATH)/soc/Android.mk
+ENABLE_QCACLD := true
+ifeq ($(TARGET_USES_QMAA), true)
+ifneq ($(TARGET_USES_QMAA_OVERRIDE_WLAN), true)
+ENABLE_QCACLD := false
+else
+ENABLE_QCACLD := true
 endif
 endif
 
-ifeq ($(call is-board-platform-in-list,sdm660),true)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd934x/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/wcd934x/Android.mk
+ifeq  ($(ENABLE_QCACLD), true)
+# Android makefile for the WLAN Module
+LOCAL_PATH := $(call my-dir)
+
+# Assume no targets will be supported
+WLAN_CHIPSET :=
+
+ifeq ($(BOARD_HAS_QCOM_WLAN), true)
+
+# Check if this driver needs be built for current target
+ifneq ($(findstring qca_cld3,$(WIFI_DRIVER_BUILT)),)
+	WLAN_CHIPSET := qca_cld3
+	WLAN_SELECT  := CONFIG_QCA_CLD_WLAN=m
 endif
 
-ifeq ($(call is-board-platform-in-list,msmnile sdmshrike),true)
-ifneq ($(TARGET_BOARD_AUTO),true)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/aqt1000/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/aqt1000/Android.mk
+# Build/Package only in case of supported target
+ifneq ($(WLAN_CHIPSET),)
+
+# This makefile is only for DLKM
+ifneq ($(findstring vendor,$(LOCAL_PATH)),)
+
+ifneq ($(findstring opensource,$(LOCAL_PATH)),)
+	WLAN_BLD_DIR := vendor/qcom/opensource/wlan
+endif # opensource
+
+# Multi-ko check
+LOCAL_DEV_NAME := $(patsubst .%,%,\
+	$(lastword $(strip $(subst /, ,$(LOCAL_PATH)))))
+
+ifeq (1, $(strip $(shell expr $(words $(strip $(TARGET_WLAN_CHIP))) \>= 2)))
+
+ifeq ($(LOCAL_DEV_NAME), qcacld-3.0)
+LOCAL_MULTI_KO := true
+else
+LOCAL_MULTI_KO := false
+endif
+
+endif
+
+ifeq ($(LOCAL_MULTI_KO), true)
+LOCAL_ANDROID_ROOT := $(shell pwd)
+LOCAL_WLAN_BLD_DIR := $(LOCAL_ANDROID_ROOT)/$(WLAN_BLD_DIR)
+$(shell find $(LOCAL_WLAN_BLD_DIR)/qcacld-3.0/ -maxdepth 1 \
+	-name '.*' ! -name '.git' -exec rm -rf {} +)
+
+$(foreach chip, $(TARGET_WLAN_CHIP), \
+	$($(shell mkdir -p $(LOCAL_WLAN_BLD_DIR)/qcacld-3.0/.$(chip); \
+	ln -sf $(LOCAL_WLAN_BLD_DIR)/qca-wifi-host-cmn \
+		$(LOCAL_WLAN_BLD_DIR)/qcacld-3.0/.$(chip)/qca-wifi-host-cmn); \
+	$(foreach node, \
+	$(shell find $(LOCAL_WLAN_BLD_DIR)/qcacld-3.0/ -maxdepth 1 \
+		! -name '.*' ! -name '*~' \
+		! -name '.' ! -name 'qcacld-3.0'), \
+	$(shell ln -sf $(node) \
+	$(LOCAL_WLAN_BLD_DIR)/qcacld-3.0/.$(chip)/$(lastword $(strip $(subst /, ,$(node)))) \
+	))))
+
+include $(foreach chip, $(TARGET_WLAN_CHIP), $(LOCAL_PATH)/.$(chip)/Android.mk)
+
+else # Multi-ok check
+
+ifeq ($(WLAN_PROFILE),)
+WLAN_PROFILE := default
+endif
+
+ifeq ($(LOCAL_DEV_NAME), qcacld-3.0)
+
+LOCAL_DEV_NAME := wlan
+LOCAL_MOD_NAME := wlan
+CMN_OFFSET := ..
+LOCAL_SRC_DIR :=
+TARGET_FW_DIR := firmware/wlan/qca_cld
+TARGET_CFG_PATH := /vendor/etc/wifi
+TARGET_MAC_BIN_PATH := /mnt/vendor/persist
+
+else
+
+LOCAL_SRC_DIR := .$(LOCAL_DEV_NAME)
+CMN_OFFSET := .
+# Use default profile if WLAN_CFG_USE_DEFAULT defined.
+ifeq ($(WLAN_CFG_USE_DEFAULT),)
+WLAN_PROFILE := $(LOCAL_DEV_NAME)
+endif
+TARGET_FW_DIR := firmware/wlan/qca_cld/$(LOCAL_DEV_NAME)
+TARGET_CFG_PATH := /vendor/etc/wifi/$(LOCAL_DEV_NAME)
+TARGET_MAC_BIN_PATH := /mnt/vendor/persist/$(LOCAL_DEV_NAME)
+
+ifneq ($(TARGET_MULTI_WLAN), true)
+LOCAL_MOD_NAME := wlan
+DYNAMIC_SINGLE_CHIP := $(LOCAL_DEV_NAME)
+else
+LOCAL_MOD_NAME := $(LOCAL_DEV_NAME)
+endif
+
+endif
+
+# DLKM_DIR was moved for JELLY_BEAN (PLATFORM_SDK 16)
+ifeq ($(call is-platform-sdk-version-at-least,16),true)
+	DLKM_DIR := $(TOP)/device/qcom/common/dlkm
+else
+	DLKM_DIR := build/dlkm
+endif # platform-sdk-version
+
+# Build wlan.ko as $(WLAN_CHIPSET)_wlan.ko
+###########################################################
+# This is set once per LOCAL_PATH, not per (kernel) module
+KBUILD_OPTIONS := WLAN_ROOT=$(WLAN_BLD_DIR)/qcacld-3.0/$(LOCAL_SRC_DIR)
+KBUILD_OPTIONS += WLAN_COMMON_ROOT=$(CMN_OFFSET)/qca-wifi-host-cmn
+KBUILD_OPTIONS += WLAN_COMMON_INC=$(WLAN_BLD_DIR)/qca-wifi-host-cmn
+KBUILD_OPTIONS += WLAN_FW_API=$(WLAN_BLD_DIR)/fw-api
+KBUILD_OPTIONS += WLAN_PROFILE=$(WLAN_PROFILE)
+KBUILD_OPTIONS += DYNAMIC_SINGLE_CHIP=$(DYNAMIC_SINGLE_CHIP)
+
+# We are actually building wlan.ko here, as per the
+# requirement we are specifying <chipset>_wlan.ko as LOCAL_MODULE.
+# This means we need to rename the module to <chipset>_wlan.ko
+# after wlan.ko is built.
+KBUILD_OPTIONS += MODNAME=$(LOCAL_MOD_NAME)
+KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
+KBUILD_OPTIONS += $(WLAN_SELECT)
+
+ifneq ($(WLAN_CFG_OVERRIDE_$(LOCAL_DEV_NAME)),)
+KBUILD_OPTIONS += WLAN_CFG_OVERRIDE="$(WLAN_CFG_OVERRIDE_$(LOCAL_DEV_NAME))"
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE              := $(WLAN_CHIPSET)_$(LOCAL_DEV_NAME).ko
+LOCAL_MODULE_KBUILD_NAME  := $(LOCAL_MOD_NAME).ko
+LOCAL_MODULE_DEBUG_ENABLE := true
+ifeq ($(PRODUCT_VENDOR_MOVE_ENABLED),true)
+    ifeq ($(WIFI_DRIVER_INSTALL_TO_KERNEL_OUT),true)
+        LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+    else
+        LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/lib/modules/$(WLAN_CHIPSET)
+    endif
+else
+    LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules/$(WLAN_CHIPSET)
+endif
+
+include $(DLKM_DIR)/AndroidKernelModule.mk
+###########################################################
+
+# Create Symbolic link
+ifneq ($(findstring $(WLAN_CHIPSET),$(WIFI_DRIVER_DEFAULT)),)
+ifeq ($(PRODUCT_VENDOR_MOVE_ENABLED),true)
+ifneq ($(WIFI_DRIVER_INSTALL_TO_KERNEL_OUT),)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/lib/modules; \
+	ln -sf /$(TARGET_COPY_OUT_VENDOR)/lib/modules/$(WLAN_CHIPSET)/$(LOCAL_MODULE) $(TARGET_OUT_VENDOR)/lib/modules/$(LOCAL_MODULE))
+endif
+else
+$(shell mkdir -p $(TARGET_OUT)/lib/modules; \
+	ln -sf /system/lib/modules/$(WLAN_CHIPSET)/$(LOCAL_MODULE) $(TARGET_OUT)/lib/modules/$(LOCAL_MODULE))
 endif
 endif
 
-ifeq ($(call is-board-platform-in-list,$(MSMSTEPPE) $(TRINKET) bengal),true)
-ifneq ($(TARGET_BOARD_AUTO),true)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/bolero/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/bolero/Android.mk
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd937x/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/wcd937x/Android.mk
-endif
+ifeq ($(PRODUCT_VENDOR_MOVE_ENABLED),true)
+TARGET_FW_PATH := $(TARGET_OUT_VENDOR)/$(TARGET_FW_DIR)
+else
+TARGET_FW_PATH := $(TARGET_OUT_ETC)/$(TARGET_FW_DIR)
 endif
 
-ifeq ($(call is-board-platform-in-list, bengal),true)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/rouleur/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/rouleur/Android.mk
+$(shell mkdir -p $(TARGET_FW_PATH); \
+	ln -sf $(TARGET_MAC_BIN_PATH)/wlan_mac.bin $(TARGET_FW_PATH)/wlan_mac.bin)
+ifneq ($(GENERIC_ODM_IMAGE),true)
+$(shell ln -sf $(TARGET_CFG_PATH)/WCNSS_qcom_cfg.ini $(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini)
 endif
-
-ifeq ($(call is-board-platform-in-list, kona lito),true)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/bolero/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/bolero/Android.mk
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd938x/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/wcd938x/Android.mk
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wcd937x/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/wcd937x/Android.mk
-endif
-
-ifeq ($(call is-board-platform-in-list, lito),true)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/wsa883x/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/wsa883x/Android.mk
-endif
-
-ifeq ($(call is-board-platform-in-list, sdm660),true)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/sdm660_cdc/Module.symvers)
-$(shell rm -rf $(PRODUCT_OUT)/obj/vendor/qcom/opensource/audio-kernel/asoc/codecs/msm_sdw/Module.symvers)
-include $(MY_LOCAL_PATH)/asoc/codecs/sdm660_cdc/Android.mk
-include $(MY_LOCAL_PATH)/asoc/codecs/msm_sdw/Android.mk
-endif
+endif # Multi-ko check
+endif # DLKM check
+endif # supported target check
+endif # WLAN enabled check
+endif # ENABLE_QCACLD
--- Makefile
+++ Makefile
@@ -1,44 +1,32 @@
-# SPDX-License-Identifier: GPL-2.0-only
+KERNEL_SRC ?= /lib/modules/$(shell uname -r)/build
 
-# auto-detect subdirs
-ifeq ($(CONFIG_ARCH_KONA), y)
-include $(srctree)/techpack/camera/config/konacamera.conf
-endif
+# The Make variable $(M) must point to the directory that contains the module
+# source code (which includes this Makefile). It can either be an absolute or a
+# relative path. If it is a relative path, then it must be relative to the
+# kernel source directory (KERNEL_SRC). An absolute path can be obtained very
+# easily through $(shell pwd). Generating a path relative to KERNEL_SRC is
+# difficult and we accept some outside help by letting the caller override the
+# variable $(M). Allowing a relative path for $(M) enables us to have the build
+# system put output/object files (.o, .ko.) into a directory different from the
+# module source directory.
+M ?= $(shell pwd)
 
-ifeq ($(CONFIG_ARCH_LITO), y)
-include $(srctree)/techpack/camera/config/litocamera.conf
-endif
+# WLAN_ROOT must contain an absolute path (i.e. not a relative path)
+KBUILD_OPTIONS := WLAN_ROOT=$(shell cd $(KERNEL_SRC); readlink -e $(M))
+KBUILD_OPTIONS += MODNAME?=wlan
 
-ifeq ($(CONFIG_ARCH_BENGAL), y)
-include $(srctree)/techpack/camera/config/bengalcamera.conf
-endif
+#By default build for CLD
+WLAN_SELECT := CONFIG_QCA_CLD_WLAN=m
+KBUILD_OPTIONS += CONFIG_QCA_WIFI_ISOC=0
+KBUILD_OPTIONS += CONFIG_QCA_WIFI_2_0=1
+KBUILD_OPTIONS += $(WLAN_SELECT)
+KBUILD_OPTIONS += $(KBUILD_EXTRA) # Extra config if any
 
-ifeq ($(CONFIG_ARCH_KONA), y)
-LINUXINCLUDE    += \
-		-include $(srctree)/techpack/camera/config/konacameraconf.h
-endif
+all:
+	$(MAKE) -C $(KERNEL_SRC) M=$(M) modules $(KBUILD_OPTIONS)
 
-ifeq ($(CONFIG_ARCH_LITO), y)
-LINUXINCLUDE    += \
-		-include $(srctree)/techpack/camera/config/litocameraconf.h
-endif
+modules_install:
+	$(MAKE) INSTALL_MOD_STRIP=1 M=$(M) -C $(KERNEL_SRC) modules_install
 
-ifeq ($(CONFIG_ARCH_BENGAL), y)
-LINUXINCLUDE    += \
-		-include $(srctree)/techpack/camera/config/bengalcameraconf.h
-endif
-
-ifdef CONFIG_SPECTRA_CAMERA
-# Use USERINCLUDE when you must reference the UAPI directories only.
-USERINCLUDE     += \
-                -I$(srctree)/techpack/camera/include/uapi
-
-# Use LINUXINCLUDE when you must reference the include/ directory.
-# Needed to be compatible with the O= option
-LINUXINCLUDE    += \
-                -I$(srctree)/techpack/camera/include/uapi \
-                -I$(srctree)/techpack/camera/include
-obj-y += drivers/
-else
-$(info Target not found)
-endif
+clean:
+	$(MAKE) -C $(KERNEL_SRC) M=$(M) clean $(KBUILD_OPTIONS)
